25d13085987062c6a39b5da3cace3060147d0981
ashibullah <ashibullahbhai@gmail.com>
2024-12-06 02:43:42 +0600

    made individual visa api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
-app.listen(port, ()=>{
-    console.log("Server is running on port: ",port);
+app.listen(port, () => {
+  console.log("Server is running on port: ", port);
 })
 
 const db_username = process.env.DB_USER;
@@ -16,9 +16,9 @@ const db_password = process.env.DB_PASS;
 
 
 const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.91k5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-app.get('/', async(req,res)=>{
-    res.send("Server is running fine");
-} )
+app.get('/', async (req, res) => {
+  res.send("Server is running fine");
+})
 
 
 
@@ -41,16 +41,26 @@ async function run() {
     const myDB = client.db("Visa-Navigator");
     const myColl = myDB.collection("Visa");
 
-    app.post('/visa', async(req,res)=>{
+    app.post('/visa', async (req, res) => {
       const visa = req.body;
       const result = await myColl.insertOne(visa);
-      console.log("New visa: ",visa)
+      console.log("New visa: ", visa)
     })
 
-  app.get('/visa', async(req,res)=>{
-    const result = await myColl.find().toArray();
-    res.send(result)
-  })    
+    app.get('/visa', async (req, res) => {
+      const result = await myColl.find().toArray();
+      res.send(result)
+    })
+    app.get('/visas/:id', async (req, res) => {
+      const id = req.params.id;
+      try {
+        const query = { _id: new ObjectId(id) };
+        const result = await myColl.findOne(query)
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Error to find visa', error });
+      }
+    })
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
